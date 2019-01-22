8d669edc9971551879836ceb1479b0b385526a81
Samuel <sumlebulle@hotmail.com>
2019-01-22 10:22:40 +0100

    Installed a bunch of npm stuff

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ const browsersync = require('rollup-plugin-browsersync')
 const postcss = require('rollup-plugin-postcss')
 const postcssNormalize = require('postcss-normalize')
 const autoprefixer = require('autoprefixer')
+const babel = require('rollup-plugin-babel')
+const nodeResolve = require('rollup-plugin-node-resolve')
+const commonJs = require('rollup-plugin-commonjs')
+const {terser} = require('rollup-plugin-terser')
 
 const isProduction = process.env.NODE_ENV === 'production';
 const isDevelopment = isProduction === false;
@@ -13,6 +17,10 @@ module.exports = {
     format: 'iife'
   },
   plugins: [
+    babel(),
+    nodeResolve(),
+    commonJs(),
+    (isProduction && terser()),
     postcss({
       plugins: [
         postcssNormalize(),
